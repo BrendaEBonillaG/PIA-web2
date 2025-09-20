9e9cfe598ea1d88ca18575537a337ff52fbb56cf
BrendaEBonillaG <82917836+BrendaEBonillaG@users.noreply.github.com>
2025-09-20 00:22:40 -0600

    incorporaciones alv

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,19 +1,20 @@
-
+import { Link } from 'react-router-dom';
 import { Login } from "../login/login";
 import Registro from "../registro/registro"; // ← importación por defecto
-
+import Favoritos from "../routes/favoritos";
+import Perfil from "../routes/perfil";
 
 // Metadata de la página
 export function meta() {
   return [
-
-    { title: "Login - React Router App" },
-    { name: "description", content: "Página de inicio de sesión" },
+    { title: "Home - React Router App" },
+    { name: "description", content: "Página de inicio de sesión o registro" },
   ];
 }
 
 // Componente principal de la página Home
 export default function Home() {
-  return <Registro />;
+  return (
+   <Perfil />
+  );
 }
-
